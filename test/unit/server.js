3475f9a5bcf71c9be8632719cc1eafaa10498f6a
Philippe Voinov <philippevoinov@gmail.com>
2016-01-27 11:39:54 +0100

    Empty server tests.

---
+++ test/unit/server.js
@@ -0,0 +1,14 @@
+"use strict";
+var _ = require('lodash'),
+  chai = require('chai'),
+  sinon = require('sinon'),
+  sinonChai = require('sinon-chai'),
+  expect = chai.expect,
+  chaiAsPromised = require('chai-as-promised'),
+  proxyquire = require('proxyquire');
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+describe('server', function () {
+});
