8b625dc940e162f432bf4b496898ff4556111573
Philippe Voinov <philippevoinov@gmail.com>
2016-01-28 18:29:23 +0100

    Added executable.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+"use strict";
+var argv = require('minimist')(process.argv.slice(2)),
+  Server = require('./server');
+
+let port = argv.port || 4420;
+let server = new Server();
+console.log('Starting walris-central');
+server.start()
+.then(() => console.log(`Listening on port ${port}.`))
+.catch(() => console.error('Failed to start.'));
